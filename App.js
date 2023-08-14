9b95b82d1c13aef7538b9b336265ce375f3d499e
Chetan Manral <137912484+ChetanManral999@users.noreply.github.com>
2023-08-14 20:39:53 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Restaurant from './component/Basics/Restaurant';
+const App=()=>{
+  return(
+    <div>
+      <Restaurant/>
+    </div>
+  );
+}
+export default App;
---
+++ menuApi.js
@@ -0,0 +1,86 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/maggi.jpg",
+      name: "maggi",
+      category: "breakfast",
+      price: "12₹",
+      description:
+      "Deliciously Fast, Maggi Lasts!",
+    },
+  
+    {
+      id: 2,
+      image: "images/allupakoida.jpg",
+      name: "allu pakoida",
+      category: "evening",
+      price: "20₹",
+      description:
+      "Crispy Crunch, Allu-Pakoda Munch!",
+    },
+    {
+      id: 3,
+      image: "images/corn.jpg",
+      name: "corn",
+      category: "breakfast",
+      price: "10₹",
+      description:
+      "Naturally Sweet, Kernel Complete.",
+    },
+    {
+      id: 4,
+      image: "../images/chola.jpg",
+      name: "chola",
+      category: "lunch",
+      price: "50₹",
+      description:
+      "Hearty Comfort, Spice of Life.",
+    },
+    {
+      id: 5,
+      image: "../images/pizza.jpg",
+      name: "pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+      "Slice of Joy, Topped with Love.",
+    },
+    {
+      id: 6,
+      image: "../images/nonvegthali.jpg",
+      name: "Non-Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+      "Savor the Flavor, Dive into Delight!",
+    },
+    {
+      id: 7,
+      image: "../images/sweet.jpg",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+      "Indulge in Bliss, Sweet Moments Await."
+    },
+    {
+      id: 8,
+      image: "../images/rajmarice.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+      "Rajma Chawal: A Classic Comfort, A Taste of Home.",
+    },
+    {
+      id: 9,
+      image: "../images/samosa.jpg",
+      name: "samaso",
+      category: "evening",
+      price: "10₹",
+      description:
+      "Triangular Delights, Spiced to Perfection!",
+    },
+  ];
+  
+  export default Menu;
